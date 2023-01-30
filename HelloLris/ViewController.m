3a9d071294ebfe1bc4eabec45b0b54dd1f67d650
lanxuexing <lanxuexing313wsr@163.com>
2023-01-30 18:51:14 +0800

    切换文字颜色

--- HelloLris/ViewController.m
+++ HelloLris/ViewController.m
@@ -9,6 +9,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic, weak)IBOutlet UILabel *label;
+
 @end
 
 @implementation ViewController
@@ -18,5 +20,22 @@
     // Do any additional setup after loading the view.
 }
 
+#pragma mark - 点击红色按钮
+- (IBAction)clickRedButton {
+    NSLog(@"%s", __func__);
+    self.label.textColor = [UIColor redColor];
+}
+
+#pragma mark - 点击绿色按钮
+- (IBAction)clickGreenButton {
+    NSLog(@"%s", __func__);
+    self.label.textColor = [UIColor greenColor];
+}
+
+#pragma mark - 点击蓝色按钮
+- (IBAction)clickBlueButton {
+    NSLog(@"%s", __func__);
+    self.label.textColor = [UIColor blueColor];
+}
 
 @end
